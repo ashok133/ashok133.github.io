00d5c969779014b958dd5bab418e245c4e416255
Ashok <ashok05@somaiya.edu>
2018-11-12 00:00:03 -0500

    Added cards on about page, finally.

--- js/bgmaploader.js
+++ js/bgmaploader.js
@@ -1,7 +1,5 @@
 function initMap() {
 
-        // Create a new StyledMapType object, passing it an array of styles,
-        // and the name to be displayed on the map type control.
         var styledMapType = new google.maps.StyledMapType(
             [
 			  {
@@ -167,45 +165,44 @@ function initMap() {
 
         var myLatlng = new google.maps.LatLng(39.17,-86.49);
 
-        // Create a map object, and include the MapTypeId to add
-        // to the map type control.
         var map = new google.maps.Map(document.getElementById('googlemapbg'), {
           center: myLatlng,
           zoom: 5,
           disableDefaultUI: true
         });
 
-  //       var marker = new google.maps.Marker({
-  //   		position: myLatlng,
-  //   		animation: google.maps.Animation.DROP
-		// });
+		var markerFileUrl = "js/Ripple.svg";
 
-		var marker, i;
+		if(document.getElementsByTagName('title')[0].innerHTML == "About - Ashok Patel") {
+			markerFileUrl = "";
+		}
+
+		console.log(document.getElementsByTagName('title')[0].innerHTML);
+			var marker, i;
 	
-	var icon = {
-				url: "js/Ripple.svg", // url 
+		var icon = {
+				url: markerFileUrl, // url 
 				scaledSize: new google.maps.Size(200, 200), // scaled size
 				origin: new google.maps.Point(0,0), // origin
 				anchor: new google.maps.Point(100,100) // anchor
 			};
 	  
-      marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map, 
-		    icon: icon,
-		    optimized: false
-      });
-	  
-	   google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
+	    marker = new google.maps.Marker({
+	      	position: myLatlng,
+	        map: map, 
+			    icon: icon,
+			    optimized: false
+	    });
+		  
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	        return function() {
+	          infowindow.setContent(locations[i][0]);
+	          infowindow.open(map, marker);
+	     	}
+	   	})(marker, i));
 
 		marker.setMap(googlemapbg);
-
         //Associate the styled map with the MapTypeId and set it to display.
         map.mapTypes.set('styled_map', styledMapType);
         map.setMapTypeId('styled_map');
-      }
+}
